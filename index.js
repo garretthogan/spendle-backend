dc95ff6f7afb37d54b1ee9cdd41a62fa4e578697
Garrett Hogan <garrett.hogan.dev@gmail.com>
2018-05-28 00:24:10 -0700

    always hit develop from plaid

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const dyanmoDb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 const tableName = 'spendle-user-data';
 
 const envs = {
-  'development': plaid.environments.sandbox,
-  'production': plaid.environments.sandbox,
+  'development': plaid.environments.development,
+  'production': plaid.environments.development,
 };
 const plaidEnv = envs[process.env.NODE_ENV];
 const plaidClient = new plaid.Client(
